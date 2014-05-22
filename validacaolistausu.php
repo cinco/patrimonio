f49fbfd382156a40eaa29f26e9079cb61ef6c1b6
Estefano  Emanoel <estefanoemanoel@Estefano-Emanoel-2.local>
2014-05-22 01:06:58 -0300

    Alteração validacaolistausu.php

--- validacaolistausu.php
+++ validacaolistausu.php
@@ -15,12 +15,31 @@
 
 <?php
 	
+	//Incluindo arquivo conexaobd.php
 	include 'conexaobd.php';
 	echo ('Você está logado com a conta: ' . $_SESSION['login']);
+	
+	//Variável recebendo o valore inserido em listausuario.php
+	$resultadometbusca = $_POST['mtbusca'];
+	
+	//Verificando o tipo de busca
+	if ($resultadometbusca == "nome"){
+		$buscapornome = "SELECT codigo_usuario, nome_completo, cpf, matricula, login, tipo_usuario WHERE nome_completo = '$resultadometbusca'";
+		$resultado = mysql_query($buscapornome, $conexao);
+		echo var_dump($resultado);
 
-	//Variáveis recebendo os valores inseridos em cadastrousuario.php
-	//$metodobusca = "SELECT codigo_usuario, cpf, matricula, login, tipo_usuario WHERE ";
-	$dadobusca = $_POST['mtbusca'];
+	} elseif ($resultadometbusca == "cpf"){
+		$buscaporcpf = "SELECT codigo_usuario, nome_completo, cpf, matricula, login, tipo_usuario WHERE cpf = '$resultadometbusca'";
+		$resultado1 = mysql_query($buscaporcpf, $conexao);
+		echo var_dump($resultado1);
+
+	} elseif ($resultadometbusca == "matricula") {
+		$buscapormatricula = "SELECT codigo_usuario, nome_completo, cpf, matricula, login, tipo_usuario WHERE matricula = '$resultadometbusca'";
+		$resultado2 = mysql_query($buscapormatricula, $conexao);
+		echo var_dump($resultado);
+	}
+
+
 
 	echo $dadobusca;
 
